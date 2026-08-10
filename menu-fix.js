08216220bd66726b6a6fa82a0f8ceaeab5961f08
Zeitgeist23 <JWigg023@gmail.com>
2026-08-10 14:54:40 -0700

    Load corrected desktop menu behavior

--- menu-fix.js
+++ menu-fix.js
@@ -1,4 +1,4 @@
 document.write('<script src="provider-name-search.js?v=2"><\/script>');
-document.write('<script src="browse-provider-search.js?v=1"><\/script>');
+document.write('<script src="browse-provider-search.js?v=2"><\/script>');
 document.write('<script src="exclusive-menu.js?v=1"><\/script>');
 document.write('<script src="menu-fix-base.js?v=6"><\/script>');

0de2d9fedb817a39e2df44cd25eaa516a4b3406e
Zeitgeist23 <JWigg023@gmail.com>
2026-08-11 10:14:01 -0700

    Load background ride submission

--- menu-fix.js
+++ menu-fix.js
@@ -2,6 +2,7 @@ document.write('<script src="provider-name-search.js?v=2"><\/script>');
 document.write('<script src="browse-provider-search.js?v=2"><\/script>');
 document.write('<script src="provider-website-popup.js?v=1"><\/script>');
 document.write('<script src="ride-booking.js?v=2"><\/script>');
+document.write('<script src="ride-submit-background.js?v=1"><\/script>');
 document.write('<script src="exclusive-menu.js?v=1"><\/script>');
 document.write('<script src="disclaimer-fix.js?v=1"><\/script>');
 document.write('<script src="menu-fix-base.js?v=6"><\/script>');

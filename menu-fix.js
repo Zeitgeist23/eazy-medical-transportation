0b01c50ef89b295eae3301c87274570adbddb16b
Zeitgeist23 <JWigg023@gmail.com>
2026-08-11 09:58:22 -0700

    Load live ride request workflow

--- menu-fix.js
+++ menu-fix.js
@@ -1,5 +1,6 @@
 document.write('<script src="provider-name-search.js?v=2"><\/script>');
 document.write('<script src="browse-provider-search.js?v=2"><\/script>');
 document.write('<script src="provider-website-popup.js?v=1"><\/script>');
+document.write('<script src="ride-booking.js?v=1"><\/script>');
 document.write('<script src="exclusive-menu.js?v=1"><\/script>');
 document.write('<script src="menu-fix-base.js?v=6"><\/script>');
